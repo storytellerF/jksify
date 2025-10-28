43d82c93778ffd0a20d437f657b869e0e7261adb
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-28 17:56:00 +0800

    add extensions

--- jksify-plugin/src/main/kotlin/com/storyteller_f/jksify/JksifyPlugin.kt
+++ jksify-plugin/src/main/kotlin/com/storyteller_f/jksify/JksifyPlugin.kt
@@ -8,6 +8,7 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.register
 import java.io.FileOutputStream
 import java.util.Base64
@@ -22,8 +23,8 @@ abstract class DecodeBase64ToStoreFileTask : DefaultTask() {
 
     @TaskAction
     fun execute() {
-        val encoded = base64Encoded.orNull
-        if (!encoded.isNullOrBlank()) {
+        val key = base64Encoded.orNull
+        if (!key.isNullOrBlank()) {
             val outputFile = outputJKSFile.get().asFile
             outputFile.parentFile?.let {
                 if (!it.exists() && !it.mkdirs()) {
@@ -33,7 +34,7 @@ abstract class DecodeBase64ToStoreFileTask : DefaultTask() {
             if (!outputFile.exists() && !outputFile.createNewFile()) {
                 throw Exception("create failed: $outputFile")
             }
-            val decodedBytes = Base64.getDecoder().decode(encoded)
+            val decodedBytes = Base64.getDecoder().decode(key)
             FileOutputStream(outputFile).use { it.write(decodedBytes) }
             println("Base64 decoded and written to: $outputFile")
         } else {
@@ -42,16 +43,29 @@ abstract class DecodeBase64ToStoreFileTask : DefaultTask() {
     }
 }
 
+interface JksifyExtension {
+    val signKey: Property<String>
+    val generatedJksFile: RegularFileProperty
+}
+
 class JksifyPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val extension = project.extensions.create<JksifyExtension>("jksify")
+        extension.signKey.convention(getenv("storyteller_f_sign_key") ?: "")
+        extension.generatedJksFile.convention(project.layout.buildDirectory.file("signing/signing_key.jks"))
+
         val decodeBase64ToStoreFileTask = project.tasks.register<DecodeBase64ToStoreFileTask>("decodeBase64ToStoreFile") {
             group = "signing"
-            base64Encoded.set(System.getenv("storyteller_f_sign_key") ?: "")
-            outputJKSFile.set(project.layout.buildDirectory.file("signing/signing_key.jks"))
+            base64Encoded.set(extension.signKey)
+            outputJKSFile.set(extension.generatedJksFile)
         }
 
         project.afterEvaluate {
             tasks.findByName("packageRelease")?.dependsOn(decodeBase64ToStoreFileTask)
         }
     }
+}
+
+fun getenv(key: String): String? {
+    return System.getenv(key.lowercase()) ?: System.getenv(key.uppercase())
 }
