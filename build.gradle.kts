5d180c2f57a5408f29a40087928980902df3cebc
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-28 16:25:02 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "com.storyteller_f"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
